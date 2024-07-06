55cc4420699fa4fa3d7b8cedbedda66d5275f8c7
HIRU1920 <heerpatel7504@gmail.com>
2024-07-06 22:17:43 +0530

    first commit

---
+++ streamlit/algo.py
@@ -0,0 +1,43 @@
+import random
+import base64
+
+def generate_keys(message):
+    message_length = len(message)
+    key_length = message_length * 16  # Assuming 16 bits per character
+    
+    public_key = random.randint(1, 2**key_length - 1)  # Generating a random public key based on message length
+    private_key_bytes = random.randbytes(16)  # Generating 16 random bytes for private key
+    private_key = base64.b64encode(private_key_bytes).decode('utf-8')  # Base64 encoding the private key
+    
+    return public_key, private_key
+
+def add_random_letters(message):
+    random_letter = chr(random.randint(33, 126))  # Generate a random printable ASCII character
+    modified_message = random_letter + message + random_letter  # Adding the random letter at the beginning and end
+    return modified_message, random_letter
+
+def remove_letters(modified_message, random_letter):
+    # Removing the added random letter at the beginning and end from the modified message
+    return modified_message[1:-1].replace(random_letter, '')
+
+
+def encrypt_message(modified_message):
+    encrypted_message = []
+    for char in modified_message:
+        ascii_value = ord(char)
+        encrypted_char = chr(ascii_value + 4)  # Add 4 to the ASCII value
+        encrypted_message.append(encrypted_char)
+    return ''.join(encrypted_message)
+
+def decrypt_message(encrypted_message):
+    decrypted_message = []
+    for char in encrypted_message:
+        ascii_value = ord(char)
+        decrypted_char = chr(ascii_value - 4)  # Subtract 4 from the ASCII value
+        decrypted_message.append(decrypted_char)
+    decrypted_message = ''.join(decrypted_message)
+    
+    random_letter = decrypted_message[0]  # Get the random letter added during encryption
+    original_message = remove_letters(decrypted_message, random_letter)
+    
+    return decrypted_message, original_message
---
+++ streamlit/algo_ui.py
@@ -0,0 +1,84 @@
+import streamlit as st
+from algo import decrypt_message, encrypt_message, remove_letters, add_random_letters, generate_keys
+import ast
+
+st.set_page_config(page_title="WazirX", layout="wide", page_icon='🔐')
+
+with st.sidebar:
+    st.image("https://ibb.co/hW9ybcC")
+    st.markdown('# 💬 Encrypt and Decrypt your messages with CryptX🔐')
+    st.markdown('''
+    ---
+    ### 💻 How does it work ?
+
+    1. Enter the text you want to encrypt and click "🔓 Encrypt Message"
+    2. A pair of public and private key will be generated
+    3. Your plaintext is now in ciphertext
+    4. Enter the ciphertext and private key in the "🗝️ Decrypt a ciphertext"
+    5. Decrypt your message
+
+    ---
+
+    ### ✨ About
+
+    **This project explores the world of cryptography, the art of secret communication. We'll unveil techniques to encrypt messages and decrypt them, ensuring only intended recipients can understand**:
+
+    - Generate keys (Public, Private)
+    - Encrypt an input message (Plaintext)
+    - Decrypt an encrypted message (Ciphertext)
+    ---
+    ''')
+
+    st.markdown('👨‍💻 Create by [**Hir Patel **](https://github.com/HIRU1920/)')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown("### 🔓.Encrypt your message")
+    message = st.text_area('Enter the message you want to encrypt:', '')
+    encrypt_btn = st.button('Encrypt Message')
+
+    if encrypt_btn and message:
+        public_key, private_key = generate_keys(message)
+
+        st.session_state['public_key'] = f'🔓 **Public key:** {public_key}'
+        st.session_state['private_key'] = f'🗝️ **Private key:** {private_key}'
+
+        st.session_state['encrypted_message'] = f"🗨️ **Encrypted Message:** {encrypt_message(message)}"
+
+    # Retrieve the encrypted message from st.session_state
+    public_key_info = st.session_state.get('public_key', '🔓 **Public key:**')
+    private_key_info = st.session_state.get('private_key', '🗝️ **Private key:**')
+    encrypted_message = st.session_state.get('encrypted_message', '🗨️ **Encrypted Message:**')
+
+    col3, col4 = st.columns(2)
+    with col3:
+        st.info(public_key_info)
+    with col4:
+        st.info(private_key_info) 
+    
+    st.error(encrypted_message)
+
+with col2:
+    st.markdown("### 🗝️ Decrypt a ciphertext")
+    decrypt_message_input = st.text_area('Enter the message you wish to decrypt:', '')
+    decrypt_key = st.text_input('Enter the private key in Base64 format:')
+
+    decrypt_btn = st.button('Decrypt Message')
+
+    try:
+        if decrypt_btn and decrypt_message_input and decrypt_key:
+            decrypted_message, original_message = decrypt_message(decrypt_message_input)
+
+            st.success(f"💬 **Decrypted Message:** {decrypted_message}")
+    except:
+        st.warning("⏪ Please enter a valid **ciphertext** and **private key**.")
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
